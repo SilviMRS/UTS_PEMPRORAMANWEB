e654bdd9ae6487ba2c22ee980279a8d5b6db37bf
Silvi Maerosa <Silvimrs08@gmail.com>
2024-11-02 22:45:57 +0700

    Delete UTS Silvi directory

--- UTS Silvi/js/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('toggle-sidebar').addEventListener('click', function() {
-    var sidebar = document.getElementById('sidebar');
-    sidebar.classList.toggle('collapsed');
-});
